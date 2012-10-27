affef07be89444859c1c432ef8e7db7aeb0da341
tam-nt3581 <tam.nt3581@sinhvien.hoasen.edu.vn>
2012-10-27 10:00:02 +0700

    them script create_table, create_constrain, insert_table Sinhvien

---
+++ DACNB_V20/script/CreateConstraint.sql
@@ -0,0 +1,30 @@
+/*-----------------Table SinhVien----------------*/
+alter table SINHVIEN add constraint SinhVien_idsinhvien_pk primary key (IDSinhVien);
+alter table SINHVIEN add constraint SINHVIEN_IDSINHVIEN_UNI UNIQUE (IDSINHVIEN);
+
+/*-------------------TABLE MON-----------------*/
+alter table MON add constraint MON_idmon_pk primary key (IDMON);
+alter table MON add constraint MON_idsinhvien_fk foreign key (IDSINHVIEN) references SINHVIEN(IDSINHVIEN);
+alter table MON Add constraint MON_IDkhoa_fk foreign key (IDKhoa) references Khoa(IDKHOA);
+alter table MON add constraint MON_idMon_UNI UNIQUE (IDMON);
+
+/*--------------------TABLE NGANH---------------*/
+alter table NGANH add constraint Nganh_idnganh_pk primary key (IDNGANH);
+alter table NGANH add constraint Nganh_idnganh_uni unique (IDNGANH)
+alter table NGANH ADD constraint Nganh_idsinhvien_fk foreign key (IDSINHVIEN) references SINHVIEN(IDSINHVIEN);
+
+/*-------------------TABLE KHOA-------------------------*/
+alter table KHOA add constraint khoa_idkhoa_pk primary key (IDKHOA);
+alter table KHOA add constraint khoa_idkhoa_uni unique (idkhoa);
+
+/*---------------------TABLE DIEM--------------------------*/
+alter table DIEM add constraint DIEM_iddiem_pk primary key (IDDIEM);
+alter table DIEM add constraint DIEM_iddiem_uni unique (IDDIEM);
+alter table DIEM ADD constraint DIEM_idmon_fk foreign key (idmon) references MON(idmon);
+
+/*rename nganh -> IDNganh table sinhvien*/
+alter table sinhvien rename column nganh to idnganh;
+
+
+
+commit;
---
+++ DACNB_V20/script/CreateTable.sql
@@ -0,0 +1,41 @@
+CREATE TABLE SinhVien(
+  IDSinhVien varchar2(10),
+  TenSV varchar2(50),
+  NgaySinh date,
+  DiaChi varchar2(200),
+  KhoaHoc varchar2(10),
+  Lop varchar2(10),
+  Nganh varchar(50)
+  );
+
+
+create table Khoa(
+  IDKhoa varchar2(10),
+  TenKhoa varchar2(50),
+  IDMon varchar2(10),
+  TruongKhoa varchar2(50)
+);
+
+create table nganh(
+  IDNganh varchar2(10),
+  TenNganh varchar2(50),
+  IDSinhVien VARCHAR2(10)
+);
+
+create table Mon(
+  IDMon varchar2(10),
+  TenMon varchar2(50),
+  IDSINHVIEN VARCHAR2(10),
+  IDKhoa Varchar2(10)
+);
+
+create table Diem(
+  IDDiem varchar2(10),
+  IDMon varchar2(10),
+  DiemQT float,
+  DiemHK float,
+  DiemTB float
+);
+
+commit;
+  
