326fc386f76d7fa747a776973f1ea9b4006d072c
tam-nt3581 <tam.nt3581@sinhvien.hoasen.edu.vn>
2012-10-27 11:39:28 +0700

    up connectionJDBC+main+ so do cac table da tao

---
+++ DACNB_V20/src/ConnectionJDBC.java
@@ -0,0 +1,96 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionJDBC {
+
+	Connection connection = null;
+	String driverName;
+	String serverName;
+	String portNumber;
+	String sid;
+	String username;
+	String password;
+	String url = "jdbc:oracle:thin:@" + this.getServerName() + ":"
+			+ this.getPortNumber() + ":" + this.getSid();
+
+	public String getDriverName() {
+		return driverName;
+	}
+
+	public void setDriverName(String driverName) {
+		this.driverName = driverName;
+	}
+
+	public String getServerName() {
+		return serverName;
+	}
+
+	public void setServerName(String serverName) {
+		this.serverName = serverName;
+	}
+
+	public String getPortNumber() {
+		return portNumber;
+	}
+
+	public void setPortNumber(String portNumber) {
+		this.portNumber = portNumber;
+	}
+
+	public String getSid() {
+		return sid;
+	}
+
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	public ConnectionJDBC(String driverName,
+			String serverName, String portNumber, String sid, String username,
+			String password, Connection connection) {
+		super();
+		this.connection = connection;
+		this.driverName = driverName;
+		this.serverName = serverName;
+		this.portNumber = portNumber;
+		this.sid = sid;
+		this.username = username;
+		this.password = password;
+	}
+
+	public boolean doConnection() {
+		try {
+			Class.forName(driverName);
+
+			connection = DriverManager.getConnection(url, username, password);
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("ClassNotFoundException : " + e.getMessage());
+			return false;
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ DACNB_V20/src/main.java
@@ -0,0 +1,8 @@
+
+public class main {
+	//IO Error: Invalid number format for port number
+	public static void main(String arg[]) {
+		ConnectionJDBC con = new ConnectionJDBC("oracle.jdbc.driver.OracleDriver","localhost","1521","XE","QLSV","12345",null);
+		System.out.println("Connection : " + con.doConnection());
+	}
+}
