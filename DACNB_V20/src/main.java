1710aabb17c5d9c1443c2e1aea6f37e29ebefab5
tam-nt3581 <tam.nt3581@sinhvien.hoasen.edu.vn>
2012-11-02 14:50:40 +0700

    -up driver oracle, class sinhvien

--- DACNB_V20/src/main.java
+++ DACNB_V20/src/main.java
@@ -1,12 +1,22 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import model.ConnectionJDBC;
+import model.SinhVien;
+
 
 public class main {
-	
-	// IO Error: Invalid number format for port number
-	public static void main(String args[]) {
-		ConnectionJDBC con = new ConnectionJDBC(
-				"oracle.jdbc.driver.OracleDriver", "localhost", "1521", "XE",
-				"QLSV", "12345");
-		System.out.println("Connection : " + con.doConnection());
+	//IO Error: Invalid number format for port number
+	public static Connection conn = null;
+	public static void main(String arg[]) {
+		try{
+			ConnectionJDBC con = new ConnectionJDBC("oracle.jdbc.driver.OracleDriver","localhost","1521","XE","QLSV","12345",null);
+			System.out.println("Connection : " + con.doConnection());
+			SinhVien sv;
+			SinhVien.selectSinhVien();
+		}catch (SQLException ex) {
+			System.out.println(ex);
+		}
 	}
 }
+
