d65b8bed376d6a1f928c1427e35d249fb9070f1a
phuc-ntt3565 <THANH PHUC@172.18.120.59>
2012-11-09 15:52:57 +0700

    Sample JFrame

--- DACNB_V20/src/view/Demogiaodien.java
+++ DACNB_V20/src/view/Demogiaodien.java
@@ -1,117 +1,115 @@
 package view;
 
-import javax.swing.JOptionPane;
-
-import org.eclipse.swt.*;
-import org.eclipse.swt.widgets.*;
-
-import controller.User;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 public class Demogiaodien {
 
+	Display display;
+	Shell shell;
+	Label Label;
+	Text Text;
+	Button Button;
+	Combo Combo;
+
 	public Demogiaodien() {
 
-		Display display = new Display();
-		final Shell shell = new Shell(display, SWT.CLOSE);
-		shell.setText("Quan Ly Sinh Vien");
-		shell.setSize(300, 200);
+		setDisplay();
 
-		Label chao = new Label(shell, SWT.LEFT | SWT.BORDER);
-		chao.setText(" Xin Chao, Moi Dang Nhap");
-		chao.setSize(145, 18);
-		chao.setLocation(80, 15);
+		setLabelChao();
+		this.Label.setText(" Xin Chao, Moi Dang Nhap");
+		this.Label.setSize(145, 18);
+		this.Label.setLocation(80, 15);
 
-		Label labelDangNhap = new Label(shell, SWT.LEFT);
-		labelDangNhap.setLocation(10, 50);
-		labelDangNhap.setSize(50, 20);
-		labelDangNhap.setText("Account");
+		setLabelAcc();
+		this.Label.setLocation(10, 50);
+		this.Label.setSize(50, 20);
+		this.Label.setText("Account");
 
-		final Text textacc = new Text(shell, SWT.LEFT | SWT.BORDER);
-		textacc.setLocation(90, 45);
-		textacc.setSize(50, 100);
-		textacc.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
-		textacc.setForeground(display.getSystemColor(SWT.COLOR_BLACK));
-		textacc.pack();
+		setTextAcc();
+		this.Text.setLocation(90, 45);
+		this.Text.setSize(50, 100);
+		this.Text.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
+		this.Text.setForeground(display.getSystemColor(SWT.COLOR_BLACK));
+		this.Text.pack();
 
-		Label labelpass = new Label(shell, SWT.LEFT);
-		labelpass.setLocation(10, 80);
-		labelpass.setSize(50, 20);
-		labelpass.setText("Password");
+		setLabelPass();
+		this.Label.setLocation(10, 80);
+		this.Label.setSize(50, 20);
+		this.Label.setText("Password");
 
-		final Text textpass = new Text(shell, SWT.LEFT | SWT.BORDER);
-		textpass.setText("");
-		textpass.setLocation(90, 75);
-		textpass.setSize(50, 50);
-		textpass.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
-		textpass.setForeground(display.getSystemColor(SWT.COLOR_BLACK));
-		textpass.setEchoChar('*');
-		textpass.pack();
+		setTextPass();
+		this.Text.setText("");
+		this.Text.setLocation(90, 75);
+		this.Text.setSize(50, 50);
+		this.Text.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
+		this.Text.setForeground(display.getSystemColor(SWT.COLOR_BLACK));
+		this.Text.setEchoChar('*');
+		this.Text.pack();
 
-		Button buttonOk = new Button(shell, SWT.CENTER);
-		buttonOk.setSize(50, 25);
-		buttonOk.setText("Login");
-		buttonOk.setLocation(150, 130);
-		buttonOk.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event) {
-				String username = textacc.getText();
-				String password = textpass.getText();
-
-				if ("".equals(username)) {
+		setButtonOk();
+		this.Button.setSize(50, 25);
+		this.Button.setText("Login");
+		this.Button.setLocation(150, 130);
 		
-					
-					MessageBox messageBox = new MessageBox(shell, SWT.OK
-							| SWT.ICON_ERROR | SWT.CANCEL);
-					messageBox
-							.setMessage("Vui Long Khai Bao Username Va Password");
-					messageBox.open();
-				}
-				if (password == "") {
-					MessageBox messageBox = new MessageBox(shell, SWT.OK
-							| SWT.ICON_WARNING | SWT.CANCEL);
-					messageBox.setMessage("Nhap Password");
-					messageBox.open();
-				} else {
-//					MessageBox messageBox = new MessageBox(shell, SWT.OK
-//							| SWT.CANCEL);
-//					messageBox.setText("Login Form");
-//					messageBox.setMessage("Welcome: " + textacc.getText());
-//					messageBox.open();
-					User user = new User();
-					if (user.checkLogin(username, password)){
-						// hello welcome
-					}
-				}
-			}
-		});
-		
+//		 buttonOk.addListener(SWT.Selection, new Listener()) {
+//		 public void handleEvent(Event event) {
+//		 String username = textacc.getText();
+//		 String password = textpass.getText();
+//		
+//		 if ("".equals(username)) {
+//		
+//
+//		 MessageBox messageBox = new MessageBox(shell, SWT.OK
+//		 | SWT.ICON_ERROR | SWT.CANCEL);
+//		 messageBox
+//		 .setMessage("Vui Long Khai Bao Username Va Password");
+//		 messageBox.open();
+//		 }
+//		 if ("".equals(password)) {
+//		 MessageBox messageBox = new MessageBox(shell, SWT.OK
+//		 | SWT.ICON_WARNING | SWT.CANCEL);
+//		 messageBox.setMessage("Nhap Password");
+//		 messageBox.open();
+//		 } else {
+//		 MessageBox messageBox = new MessageBox(shell, SWT.OK
+//		 | SWT.CANCEL);
+//		messageBox.setText("Login Form");
+//		 messageBox.setMessage("Welcome: " + textacc.getText());
+//		 messageBox.open();
+//		 }
+//		 }
+//		 
+//
+//		 User user = new User();
+//		 if (user.checkLogin(username, password)){
+//		 hello welcome
+//		 }
+//		});
 
+		setButtonCancel();
+		this.Button.setSize(50, 25);
+		this.Button.setText("Cancel");
+		this.Button.setLocation(225, 130);
 
-		Button buttonCancel = new Button(shell, SWT.CENTER);
-		buttonCancel.setSize(50, 25);
-		buttonCancel.setText("Cancel");
-		buttonCancel.setLocation(225, 130);
-		
-
-		Button buttonluu = new Button(shell, SWT.RADIO);
-		buttonluu.setLocation(220, 104);
-		buttonluu.setText("Ghi Nho");
-		buttonluu.setSize(90, 20);
+		setButtonGhiNho();
+		this.Button.setLocation(220, 104);
+		this.Button.setText("Ghi Nho");
+		this.Button.setSize(90, 20);
 
 		String[] data = { "Sinh Vien", "Giang Vien", "Phong Dao Tao" };
-		Combo combo = new Combo(shell, SWT.DROP_DOWN);
-		combo.setItems(data);
-		combo.select(1);
-		combo.setItems(data);
-		combo.setSize(90, 20);
-		combo.setLocation(10, 130);
-		combo.setText("Sinh Vien");
-
-		// Text Doipass = new Text(shell, SWT.CENTER | SWT.WRAP);
-		// Doipass.setText("Doi Mat Khau");
-		// Doipass.setSize(80, 15);
-		// Doipass.setLocation(50, 100);
-		// Doipass.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
-		// Doipass.setForeground(display.getSystemColor(SWT.COLOR_BLACK));
+		setCombo();
+		this.Combo.setItems(data);
+		this.Combo.select(1);
+		this.Combo.setItems(data);
+		this.Combo.setSize(90, 20);
+		this.Combo.setLocation(10, 130);
+		this.Combo.setText("Sinh Vien");
 
 		shell.open();
 
@@ -120,6 +118,49 @@ public class Demogiaodien {
 				display.sleep();
 		}
 		display.dispose();
+	}
+
+	public void setDisplay() {
+		this.display = new Display();
+		this.shell = new Shell(display, SWT.CLOSE);
+		this.shell.setText("Quan Ly Sinh Vien");
+		this.shell.setSize(300, 200);
+	}
+
+	public void setLabelChao() {
+		this.Label = new Label(shell, SWT.LEFT | SWT.BORDER);
+	}
+
+	public void setLabelAcc() {
+		this.Label = new Label(shell, SWT.LEFT);
+	}
+
+	public void setTextAcc() {
+		this.Text = new Text(shell, SWT.LEFT | SWT.BORDER);
+	}
+
+	public void setLabelPass() {
+		this.Label = new Label(shell, SWT.LEFT);
+	}
+
+	public void setTextPass() {
+		this.Text = new Text(shell, SWT.LEFT | SWT.BORDER);
+	}
+
+	public void setButtonOk() {
+		this.Button = new Button(shell, SWT.CENTER);
+	}
+
+	public void setButtonCancel() {
+		this.Button = new Button(shell, SWT.CENTER);
+	}
+
+	public void setButtonGhiNho() {
+		this.Button = new Button(shell, SWT.RADIO);
+	}
+
+	public void setCombo() {
+		this.Combo = new Combo(shell, SWT.DROP_DOWN);
 	}
 
 	public static void main(String args[]) {
