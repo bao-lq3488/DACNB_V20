6edc13ee607b700e70fb131b761e16827e51e402
tam-nt3581 <tam.nt3581@sinhvien.hoasen.edu.vn>
2012-11-09 11:25:03 +0700

    update class SinhVien: su dung get set va arraylist

--- DACNB_V20/src/model/SinhVien.java
+++ DACNB_V20/src/model/SinhVien.java
@@ -10,7 +10,72 @@ import java.util.ArrayList;
 public class SinhVien {
 	public static ConnectionJDBC conn = null;
 
+	public String getIDSinhVien() {
+		return IDSinhVien;
+	}
+
+	public void setIDSinhVien(String iDSinhVien) {
+		IDSinhVien = iDSinhVien;
+	}
+
+	public String getTenSinhVien() {
+		return TenSinhVien;
+	}
+
+	public void setTenSinhVien(String tenSinhVien) {
+		TenSinhVien = tenSinhVien;
+	}
+
+	public String getNgaySinh() {
+		return NgaySinh;
+	}
+
+	public void setNgaySinh(String ngaySinh) {
+		NgaySinh = ngaySinh;
+	}
+
+	public String getDiaChi() {
+		return DiaChi;
+	}
+
+	public void setDiaChi(String diaChi) {
+		DiaChi = diaChi;
+	}
+
+	public String getKhoaHoc() {
+		return KhoaHoc;
+	}
+
+	public void setKhoaHoc(String khoaHoc) {
+		KhoaHoc = khoaHoc;
+	}
+
+	public String getLop() {
+		return Lop;
+	}
+
+	public void setLop(String lop) {
+		Lop = lop;
+	}
+
+	public String getNganh() {
+		return Nganh;
+	}
+
+	public void setNganh(String nganh) {
+		Nganh = nganh;
+	}
+
+	String IDSinhVien;
+	String TenSinhVien;
+	String NgaySinh;
+	String DiaChi;
+	String KhoaHoc;
+	String Lop;
+	String Nganh;
+
 	public static void selectSinhVien() throws SQLException {
+		ArrayList<SinhVien> svList = new ArrayList<SinhVien>();
 		ConnectionJDBC conn = new ConnectionJDBC(
 				"oracle.jdbc.driver.OracleDriver", "localhost", "1521", "XE",
 				"QLSV", "12345", null);
@@ -22,17 +87,21 @@ public class SinhVien {
 			stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(qry);
 			while (rs.next()) {
-				String IDSinhVien = rs.getString("IDSINHVIEN");
-				String TenSinhVien = rs.getString("TENSV");
-				String NgaySinh = rs.getString("NGAYSINH");
-				String DiaChi = rs.getString("DIACHI");
-				String KhoaHoc = rs.getString("KHOAHOC");
-				String Lop = rs.getString("LOP");
-				String Nganh = rs.getString("IDNGANH");
-				System.out.println(IDSinhVien + "     " + TenSinhVien + "    "
-						+ NgaySinh + "   " + DiaChi + "    " + KhoaHoc + "    "
-						+ Lop + "   " + Nganh);
+				SinhVien sv = new SinhVien();
+				sv.setIDSinhVien(rs.getString("IDSINHVIEN"));
+				sv.setTenSinhVien(rs.getString("TENSV"));
+				sv.setNgaySinh(rs.getString("NGAYSINH"));
+				sv.setDiaChi(rs.getString("DIACHI"));
+				sv.setKhoaHoc(rs.getString("KHOAHOC"));
+				sv.setLop(rs.getString("LOP"));
+				sv.setNganh(rs.getString("IDNGANH"));
+				 System.out.println(sv.getIDSinhVien() + "     "
+				 + sv.getTenSinhVien() + "    " + sv.getNgaySinh()
+				 + "   " + sv.getDiaChi() + "    " + sv.getKhoaHoc()
+				 + "    " + sv.getLop() + "   " + sv.getNganh());
+				svList.add(sv);
 			}
+	
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -55,17 +124,5 @@ public class SinhVien {
 		}
 
 	}
-	public static void arrayListSinhVien(){
-	
-		ArrayList<Object> arl = new ArrayList<Object>();
-//		String IDSinhVien = new String();
-//		String TenSinhVien = new String();
-//		String NgaySinh = new String();
-//		String DiaChi = new String();
-//		String KhoaHoc = new String();
-//		String Lop = new String();
-//		String Nganh = new String();
-		
-		
-	}
+
 }
