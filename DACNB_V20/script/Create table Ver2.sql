08741322c4c9a55008e6afa94c564f1ef20c826c
quang-tb0078 <quanrock2000@yahoo.com>
2012-11-15 12:09:37 +0700

    tiep tuc database coding

--- DACNB_V20/script/Create table Ver2.sql
+++ DACNB_V20/script/Create table Ver2.sql
@@ -1,4 +1,4 @@
-create type SinhVien as object(
+CREATE TYPE SinhVien AS OBJECT(
   IDSinhVien varchar(10),
   TenSV varchar(50),
   NgaySinh date,
@@ -6,40 +6,77 @@ create type SinhVien as object(
   KhoaHoc varchar(10),
   Lop varchar(10),
   Nganh varchar(50)
+)
+CREATE TYPEe SinhVien1 AS TABLE OF SinhVien;
+create table SinhVien2 of SinhVien(
+  IDSinhVien not null,
+  primary key (IDSinhVien)
 );
-create type SinhVien_B as table of SinhVien;
-create table SinhVien_HS(
-SinhVienHS SinhVien
+create table SinhVien3(
+  SinhVienHS SinhVien
+  ...ref...
 );
 
 create type Khoa as object(
   IDKhoa varchar(10),
   TenKhoa varchar(50),
-  IDMon varchar(10),
   TruongKhoa varchar(50)
 );
-create type Khoa_B as table of Khoa;
+create type Khoa1 as table of Khoa;
+create table Khoa2 of Khoa(
+  IDKhoa not null,
+  primary key(IDKhoa)
+);
+create table Khoa3(
+  KhoaHS Khoa
+...ref...
+);
 
 create Nganh as object(
   IDNganh varchar(10),
   TenNganh varchar(50),
-  IDSinhVien VARCHAR(10)
 );
-create type Nganh_B as table of Nganh;
+create type Nganh1 as table of Nganh;
+create table Nganh2 of Nganh(
+  IDNganh not null,
+  primary key (IDNganh),
+  foreign key (IDSinhVien) reference SinhVien2(IDSinhVien)
+);
+create table Nganh3(
+  NganhHS Nganh
+  ...ref...
+);
 
 create type Mon as object(
-IDMon varchar(10),
+  IDMon varchar(10),
   TenMon varchar(50),
-  IDSinhVien VARCHAR(10),
-  IDKhoa Varchar(10)
 );
-create type Mon_B as table of Mon;
+create type Mon1 as table of Mon;
+create table Mon2 of Mon(
+  IDMon not null,
+  primary key (IDMon),
+  foreign key (IDKhoa) reference Khoa(IDKhoa),
+  foreign key (IDSinhVien) reference SinhVien(IDSinhVien),
+);
+create table Mon3(
+  MonHS Mon
+  ...ref...
+);
 
 create type Diem as object(
-IDDiem varchar(10),
   IDMon varchar(10),
   DiemQT float,
   DiemHK float,
   DiemTB float
 );
-create type Diem_B as table of Diem;
+create type Diem1 as table of Diem;
+create table Diem2 of Diem(
+  IDMon not null,
+  primary key (IDMon),
+  foreign key (IDMon) reference Mon(IDMon)
+);
+create table Diem3(
+  DiemHS Diem,
+  ...ref...
+);
+commit;
