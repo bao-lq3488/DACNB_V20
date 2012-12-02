ab535505e307bd242bfabe7c0b1b298f0e859868
bao-lq3488 <Hishoka@Hishoka-Dragon>
2012-12-02 18:37:56 +0700

    Tối ưu code Form_LietKe
    Signed-off-by: bao-lq3488 <Hishoka@Hishoka-Dragon>

--- DACNB_V20/src/view/Form_LietKe.java
+++ DACNB_V20/src/view/Form_LietKe.java
@@ -7,59 +7,48 @@ import org.eclipse.swt.widgets.Shell;
 public class Form_LietKe {
 
 	Shell shell;
-	Label label;
 
 	public Form_LietKe(Shell mainShell) {
 
 		shell = new Shell(mainShell, SWT.APPLICATION_MODAL | SWT.CLOSE);
-		shell.setText("Liet Ke");
-
+		
 		createUI();
 
-		shell.setLocation(10, 10);
-
 		shell.open();
-
-//		while (!shell.isDisposed()) {
-//			if (!display.readAndDispatch()) {
-//				display.sleep();
-//			}
-//		}
-//		display.dispose();
 	}
 
 	private void createUI() {
 		
 		shell.setSize(300, 180);
+		shell.setText("Liet Ke");
+		shell.setLocation(10, 10);
 		
-		Label lbTenMH = new Label(shell, SWT.CENTER | SWT.BORDER);
-		lbTenMH.setText("Ten Mon Hoc");
-		lbTenMH.setSize(85, 20);
-		lbTenMH.setLocation(5, 10);
+		Label lblTenMH = new Label(shell, SWT.CENTER | SWT.BORDER);
+		lblTenMH.setText("Ten Mon Hoc");
+		lblTenMH.setSize(85, 20);
+		lblTenMH.setLocation(5, 10);
 		
-		Label lbKeDoc1 = new Label(shell, SWT.SEPARATOR | SWT.VERTICAL);
-		lbKeDoc1.setLocation(90, 0);
-		lbKeDoc1.setSize(30, 200);
+		Label lblKeDoc1 = new Label(shell, SWT.SEPARATOR | SWT.VERTICAL);
+		lblKeDoc1.setLocation(90, 0);
+		lblKeDoc1.setSize(30, 200);
 		
-		Label lbKiHieu = new Label(shell, SWT.CENTER | SWT.BORDER);
-		lbKiHieu.setText("Ki Hieu");
-		lbKiHieu.setSize(45, 20);
-		lbKiHieu.setLocation(120, 10);
+		Label lblKiHieu = new Label(shell, SWT.CENTER | SWT.BORDER);
+		lblKiHieu.setText("Ki Hieu");
+		lblKiHieu.setSize(45, 20);
+		lblKiHieu.setLocation(120, 10);
 		
-		Label lbKeDoc2 = new Label(shell, SWT.SEPARATOR | SWT.VERTICAL);
-		lbKeDoc2.setLocation(160, 0);
-		lbKeDoc2.setSize(30, 200);
+		Label lblKeDoc2 = new Label(shell, SWT.SEPARATOR | SWT.VERTICAL);
+		lblKeDoc2.setLocation(160, 0);
+		lblKeDoc2.setSize(30, 200);
 		
-		Label lbTC = new Label(shell, SWT.CENTER | SWT.BORDER);
-		lbTC.setText("So Tin Chi");
-		lbTC.setSize(70, 20);
-		lbTC.setLocation(190, 10);
+		Label lblTC = new Label(shell, SWT.CENTER | SWT.BORDER);
+		lblTC.setText("So Tin Chi");
+		lblTC.setSize(70, 20);
+		lblTC.setLocation(190, 10);
 		
-		Label lbKeNgang = new Label(shell, SWT.SEPARATOR | SWT.HORIZONTAL
+		Label lblKeNgang = new Label(shell, SWT.SEPARATOR | SWT.HORIZONTAL
 		| SWT.SHADOW_OUT);
-		lbKeNgang.setLocation(0, 20);
-		lbKeNgang.setSize(300, 30);
+		lblKeNgang.setLocation(0, 20);
+		lblKeNgang.setSize(300, 30);
 	}
-
-
 }
