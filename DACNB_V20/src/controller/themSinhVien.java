164ae439000d0dcce9991d0fb9e9762349bc0ebd
tam-nt3581 <tam.nt3581@sinhvien.hoasen.edu.vn>
2012-12-19 11:07:41 +0700

    - fix code control themsinhvien de map voi view
    Signed-off-by: tam-nt3581 <tam.nt3581@sinhvien.hoasen.edu.vn>

--- DACNB_V20/src/controller/themSinhVien.java
+++ DACNB_V20/src/controller/themSinhVien.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
+import org.eclipse.swt.widgets.Shell;
+
+import view.Form_ThemSV;
+
 import model.ConnectionJDBC;
 
 public class themSinhVien {
@@ -11,28 +15,34 @@ public class themSinhVien {
 	static ConnectionJDBC conn = new ConnectionJDBC(
 			"oracle.jdbc.driver.OracleDriver", "localhost", "1521", "XE",
 			"QLSV", "12345", null);
+	private static boolean result;
 
-	public static void main(String[] args) {
+	public boolean isResult() {
+		return result;
+	}
+
+	public static boolean setResult(boolean result) {
+		return themSinhVien.result = result;
+	}
+
+	public static boolean doAddSinhVien() {
+
 		try {
 			Connection con = DriverManager.getConnection(conn.url,
 					conn.getUsername(), conn.getPassword());
 			System.out.println("Connection : " + conn.doConnection());
 			Statement stmt = con.createStatement();
-			String query = "INSERT INTO SINHVIEN (IDSINHVIEN,TENSINHVIEN,NGAYSINH,DIACHI,STATUS,IDLOP,DATEJOIN,DATEEND) " +
-					"VALUES ('093111','phan hoang dung',to_date('1991/01/01','YYYY/MM/DD'),'HCM','DANG HOC','QL091A','2009','2012')";
-
-			// for (int i=0;i<args.length;i++)
-			// {
-			// query+= args[i];
-			// if(i+1 < args.length)
-			// query += "','";
-			// }
-			// query +="')";
+			String query = "INSERT INTO SINHVIEN (IDSINHVIEN,TENSINHVIEN,NGAYSINH,DIACHI,STATUS,IDLOP,DATEJOIN,DATEEND) "
+					+ "VALUES ('"+ "','phan hoang dung',to_date('1991/01/01','YYYY/MM/DD'),'HCM','DANG HOC','QL091A','2009','2012')";
 			stmt.executeUpdate(query);
+			return setResult(true);
 
 		} catch (Exception e) {
 			System.out.println("Got an exception! ");
 			System.err.println(e.getMessage());
+			return setResult(false);
 		}
+
 	}
+
 }
